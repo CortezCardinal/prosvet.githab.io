769874075fcfb62496bc994aadf0bcebe60c1d47
AlexeyTelenik <51457024+CortezCardinal@users.noreply.github.com>
2019-11-14 17:57:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,134 @@
+var button = document.getElementById("button");
+	 		var button1 = document.getElementById("button1");
+	 	 
+	 		var button3 = document.getElementById("button3");
+	 		var button4 = document.getElementById("button4");
+	 		var button5 = document.getElementById("button5");
+	 		var button6 = document.getElementById("button6");
+	 		button.addEventListener('click',function(){
+			 	swal({
+		  	text: 'Введите свой E-mail:',
+		  			content: 
+		  			{
+		    			element: "input",
+		    			attributes: 
+		    			{
+		      				placeholder: "E-mail:",	 
+		      			},
+					},
+		  	button:
+		  		{
+		    		text: "Отрпвить",
+		    		closeModal: true,
+		    	}, 
+});
+});
+	 			var button2 = document.getElementById("button2");
+	 		button2.addEventListener('click',function(){
+			 	swal({
+		  	text: 'Введите свой E-mail:',
+		  			content: 
+		  			{
+		    			element: "input",
+		    			attributes: 
+		    			{
+		      				placeholder: "E-mail:",	 
+		      			},
+					},
+		  	button:
+		  		{
+		    		text: "Отрпвить",
+		    		closeModal: true,
+		    	}, 
+});
+});
+	 		button2.addEventListener('click',function(){
+			 	swal({
+		  	text: 'Введите свой E-mail:',
+		  			content: 
+		  			{
+		    			element: "input",
+		    			attributes: 
+		    			{
+		      				placeholder: "E-mail:",	 
+		      			},
+					},
+		  	button:
+		  		{
+		    		text: "Отрпвить",
+		    		closeModal: true,
+		    	}, 
+});
+});
+	 		button3.addEventListener('click',function(){
+			 	swal({
+		  	text: 'Введите свой E-mail:',
+		  			content: 
+		  			{
+		    			element: "input",
+		    			attributes: 
+		    			{
+		      				placeholder: "E-mail:",	 
+		      			},
+					},
+		  	button:
+		  		{
+		    		text: "Отрпвить",
+		    		closeModal: true,
+		    	}, 
+});
+});
+	 		button4.addEventListener('click',function(){
+			 	swal({
+		  	text: 'Введите свой E-mail:',
+		  			content: 
+		  			{
+		    			element: "input",
+		    			attributes: 
+		    			{
+		      				placeholder: "E-mail:",	 
+		      			},
+					},
+		  	button:
+		  		{
+		    		text: "Отрпвить",
+		    		closeModal: true,
+		    	}, 
+});
+});
+	 		button5.addEventListener('click',function(){
+			 	swal({
+		  	text: 'Введите свой E-mail:',
+		  			content: 
+		  			{
+		    			element: "input",
+		    			attributes: 
+		    			{
+		      				placeholder: "E-mail:",	 
+		      			},
+					},
+		  	button:
+		  		{
+		    		text: "Отрпвить",
+		    		closeModal: true,
+		    	}, 
+});
+});
+	 		button6.addEventListener('click',function(){
+			 	swal({
+		  	text: 'Введите свой E-mail:',
+		  			content: 
+		  			{
+		    			element: "input",
+		    			attributes: 
+		    			{
+		      				placeholder: "E-mail:",	 
+		      			},
+					},
+		  	button:
+		  		{
+		    		text: "Отрпвить",
+		    		closeModal: true,
+		    	}, 
+});
+});
